0ec77ab0673edf96e6003c926e1da5ba75f611aa
Mikaela Hedberg <mikaela.hedberg@plandigital.se>
2020-11-12 19:34:27 +0100

    begin routing

--- blog/src/app/app-routing.module.ts
+++ blog/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ArchivesComponent } from './components/archives/archives.component';
+import { PostsDetailComponent } from './components/posts-detail/posts-detail.component';
+import { PostsComponent } from './components/posts/posts.component';
+import { UsersDetailComponent } from './components/users-detail/users-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', component: PostsComponent },
+    { path: 'posts/:id', component: PostsDetailComponent },
+    { path: 'users/:id', component: UsersDetailComponent },
+    { path: 'archives', component: ArchivesComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- blog/src/app/app.module.ts
+++ blog/src/app/app.module.ts
@@ -10,9 +10,19 @@ import { PostsComponent } from './components/posts/posts.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
 import { PostService } from './services/post.service';
 import { AppErrorHandler } from './common/app-error-handler';
+import { UsersDetailComponent } from './components/users-detail/users-detail.component';
+import { PostsDetailComponent } from './components/posts-detail/posts-detail.component';
+import { ArchivesComponent } from './components/archives/archives.component';
 
 @NgModule({
-    declarations: [AppComponent, PostsComponent, NavigationComponent],
+    declarations: [
+        AppComponent,
+        PostsComponent,
+        NavigationComponent,
+        UsersDetailComponent,
+        PostsDetailComponent,
+        ArchivesComponent
+    ],
     imports: [
         BrowserModule,
         AppRoutingModule,
