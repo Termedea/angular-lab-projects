fc5cf47ebaafc7f4a786796a19d745563d396bc7
Mikaela Hedberg <mikaela.hedberg@plandigital.se>
2020-11-13 19:14:23 +0100

    styling

--- blog/src/app/components/posts/posts.component.ts
+++ blog/src/app/components/posts/posts.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PostService } from 'src/app/services/post.service';
+import * as faker from 'faker';
 
 @Component({
     selector: 'posts',
@@ -15,6 +16,20 @@ export class PostsComponent implements OnInit {
         this._service.getAll().subscribe((posts) => {
             console.log(posts);
             this.posts = posts as any[];
+
+            this.fakeSomeData();
+            for (let post of this.posts) {
+                post.content = faker.lorem.paragraphs();
+                console.log(post);
+            }
         });
+    }
+
+    private fakeSomeData() {
+        for (let post of this.posts) {
+            post.content = faker.lorem.paragraphs();
+            post.caption = faker.lorem.paragraph();
+            console.log(post);
+        }
     }
 }
